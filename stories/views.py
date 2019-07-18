7326ec8e99e201ed0941b7e214c350236c465ec8
nisseem <niscmagar@gmail.com>
2019-07-18 15:46:02 +0545

    view milena

--- stories/views.py
+++ stories/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.template import loader
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the polls index.")
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('stories/front/index.html')
+    return HttpResponse(template.render(context, request))
